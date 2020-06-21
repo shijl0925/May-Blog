fbbb4567ce6e5753731d308a3fcf3d53f8cbcd03
jialiang.shi <jialiang.shi@wz-inc.com>
2020-06-21 20:49:13 +0800

    bug fix

--- app/controller/signals.py
+++ app/controller/signals.py
@@ -16,13 +16,16 @@ def on_post_visited(sender, post, **extra):
     nginx_remote = "X-Forwarded-For"
     ip = (flask.request.headers[nginx_remote] if nginx_remote in flask.request.headers else flask.request.remote_addr)
 
-    tracker = Tracker(
-        url=flask.request.path,
-        ip=ip,
-        post=post
-    )
-    db.session.add(tracker)
-    db.session.commit()
+    search_tracker = Tracker.query.filter_by(ip=ip, post=post).first()
 
-    post.visit_count += 1
-    db.session.commit()
+    if search_tracker is None:
+        tracker = Tracker(
+            url=flask.request.path,
+            ip=ip,
+            post=post
+        )
+        db.session.add(tracker)
+        db.session.commit()
+
+        post.visit_count += 1
+        db.session.commit()
